e7f837d221f53332669fdc825fc09ea3817bf99c
Muhammad Salah <nusair.muhammad@gmail.com>
2026-09-25 16:53:12 +0100

    Update transformer.py

--- src/transformer.py
+++ src/transformer.py
@@ -19,13 +19,3 @@ def transform_weather(raw_data: list[dict]) -> pd.DataFrame:
         records.append(clean)
     logger.info("Transformed weather data for %d cities", len(records))
     return pd.DataFrame(records)
-
-
-# test 
-# if __name__ == "__main__":
-#     test_data = [
-#         {'name': 'Cairo', 'main': {'temp': 304.57, 'feels_like': 303.9, 'temp_max': 304.57, 'temp_min': 300.0, 'humidity': 35}, 'wind': {'speed': 4.12}},
-#         {'name': 'Lisbon', 'main': {'temp': 301.75, 'feels_like': 301.89, 'temp_max': 306.82, 'temp_min': 299.62, 'humidity': 46}, 'wind': {'speed': 4.12}},
-#     ]
-#     df = transform_weather(test_data)
-#     print(df)
